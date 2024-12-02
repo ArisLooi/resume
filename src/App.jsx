229e90d2a9fbf4d30813c4d3c91a4d75feb4d15f
Aris Looi <arislooi@outlook.com>
2024-12-02 14:14:30 +0800

    initiate files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <h1>Aris Looi</h1>
+      <h1>Software developer</h1>
+      <Experience />
+    </>
+  )
+}
+
+export default App;
